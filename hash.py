de9c67a0d925041eb17ed5a18a0f72a46d32165c
vijval-studio <166107455+vijval-studio@users.noreply.github.com>
2024-11-26 15:54:01 +0530

    Delete hash.py

--- hash.py
+++
@@ -1,7 +0,0 @@
-from flask_bcrypt import Bcrypt
-
-bcrypt = Bcrypt()
-
-password = input("Enter password: ")
-hashed = bcrypt.generate_password_hash(password).decode('utf-8')
-print("Hashed password: ", hashed)
